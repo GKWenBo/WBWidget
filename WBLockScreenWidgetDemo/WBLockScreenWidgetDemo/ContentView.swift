db80dd94ac291d213734e679b4c94b3a16fb6b3a
wenbo <wenbo22@staff.weibo.com>
2022-10-18 19:58:21 +0800

    添加demo

---
+++ WBLockScreenWidgetDemo/WBLockScreenWidgetDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WBLockScreenWidgetDemo
+//
+//  Created by wenbo22 on 2022/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WBLockScreenWidgetDemo/WBLockScreenWidgetDemo/WBLockScreenWidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBLockScreenWidgetDemoApp.swift
+//  WBLockScreenWidgetDemo
+//
+//  Created by wenbo22 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct WBLockScreenWidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
